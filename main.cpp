39501b9ae860fdfe0d3cdbd410265f4a1d90a8e3
Joseph <josephmtindall@gmail.com>
2017-04-09 00:16:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
+#include "npc.h"
 using namespace std;
 
 int main()
 {
-  
-  
-  return 0;
+	NonPlayerControlled bot("Kanna", vector<string>(10)={"d2","c2","s2","d7","d8","d9","h9","h10","h11","h12"}, true);
+	bot.Play("d13");
+
+	cout << "\n"; system("pause");
+	return 0;
 }

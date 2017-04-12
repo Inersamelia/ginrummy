ec9c87f16b118a3c047cd211218bc4269aed5a4d
Joseph <josephmtindall@gmail.com>
2017-04-11 23:02:24 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main()
 {
-	NonPlayerControlled bot("Kanna", vector<string>(10)={"d2","c2","s2","d7","d8","d9","h9","h10","h11","h12"}, true);
-	bot.Play("d13");
+	cout << "What name would you grant your opponent?\n";
+	string name = "";
+	getline(cin, name);
+
+	NonPlayerControlled bot(name, vector<string>(10)={"d2","c2","s2","h1","d3","d4","h10","h11","h12","s1"}, true);
+	bot.Play("h13");
+
+	cout << "\nBot's deck contains:\n";
+		bot.OutputHand();
 
 	cout << "\n"; system("pause");
 	return 0;
